a8d65794b6bfa7a0d98992196ceeae87bb8de53f
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-08-27 18:56:18 -0300

    mudanças

--- 01.py
+++ 01.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def pagina_inicial(): 
-    return render_template("pagina-inicial.html") 
+    return render_template("index.html") 
 
 @app.route("/alunos")
 def alunos():
